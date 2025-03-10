1f04384cbb91a9b0157cdbc19a774ca45af6d29b
ggabi40 <gabibc2000@hotmail.com>
2025-03-10 17:37:17 -0300

    basic ✅

---
+++ assets/js/renderer.js
@@ -0,0 +1,44 @@
+let timerInterval;
+let isPaused = false;
+let timeRemaining;
+let workTime;
+let restTime;
+
+function startTimer(work, rest) {
+    workTime = work;
+    restTime = rest;
+    clearInterval(timerInterval);
+    let time = work * 60;
+    timeRemaining = time;
+    updateDisplay(time);
+    timerInterval = setInterval(() => {
+        if (!isPaused) {
+            timeRemaining--;
+            updateDisplay(timeRemaining);
+            if (timeRemaining <= 0) {
+                clearInterval(timerInterval);
+                alert(`Tiempo de descanso: ${rest} minutos`);
+            }
+        }
+    }, 1000);
+}
+
+function updateDisplay(time) {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    document.getElementById('timer').innerText = 
+        `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+}
+
+function pauseTimer() {
+    isPaused = !isPaused;
+    document.getElementById('pause').innerText = isPaused ? 'Reanudar' : 'Pausar';
+}
+
+function stopTimer() {
+    clearInterval(timerInterval);
+    document.getElementById('timer').innerText = '00:00';
+    isPaused = false;
+    document.getElementById('pause').innerText = 'Pausar';
+}
+
---
+++ main.js
@@ -0,0 +1,17 @@
+import { app, BrowserWindow } from 'electron';
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width:400,
+        height:600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+    createWindow();
+});
