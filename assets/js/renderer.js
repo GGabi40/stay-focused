a13eda394f4a625d7b8c8d80cc6685b4ca51df6a
ggabi40 <gabibc2000@hotmail.com>
2025-03-11 19:15:05 -0300

    ✅ animation added

--- assets/js/renderer.js
+++ assets/js/renderer.js
@@ -8,6 +8,85 @@ document.addEventListener("visibilitychange", () => {
 });
 
 
+/* Modal - Custom Timer */
+const dialogBox = document.getElementById('dialogBox');
+function showModal() {
+    dialogBox.showModal();
+    setTimeout(() => {
+        dialogBox.style.opacity = "1";
+        dialogBox.style.transform = "scale(1)";
+    }, 10);
+}
+
+function closeModal() {
+    dialogBox.style.opacity = "0";
+    dialogBox.style.transform = "scale(0.8)";
+    setTimeout(() => {
+        dialogBox.close();
+    }, 300);
+}
+
+function processing() {
+    const customTimeWork = document.getElementById('work').value;
+    const customTimeRest = document.getElementById('rest').value;
+    const error = document.getElementById('error');
+    error.classList.remove('show');
+    error.innerText = '';
+
+    if(!customTimeWork || !customTimeRest) {
+        error.classList.add('show');
+        error.innerText = 'Debes ingresar un tiempo de concentración y de descanso.';
+        return;
+    }
+
+    if(customTimeWork < 1 || customTimeRest < 1) {
+        error.classList.add('show');
+        error.innerText = 'El tiempo no debe ser negativo.';
+        return;
+    }
+
+    if (customTimeWork.trim() === '' || customTimeRest.trim() === '') {
+        error.classList.add('show');
+        error.innerText = 'No puedes ingresar solo espacios en los campos.';
+        return;
+    }
+
+    if(customTimeWork === customTimeRest) {
+        error.classList.add('show');
+        error.innerText = 'Los tiempos deben ser distintos entre sí.';
+        return;
+    }
+
+    if(customTimeWork <= 10) {
+        error.classList.add('show');
+        error.innerText = 'El tiempo de Concentración debe ser mayor a 10 minutos.';
+        return;
+    }
+
+    if (isNaN(customTimeWork) || isNaN(customTimeRest)) {
+        error.classList.add('show');
+        error.innerText = 'Solo se permiten números.';
+        return;
+    }
+
+    if (customTimeWork > 180 || customTimeRest > 180) {
+        error.classList.add('show');
+        error.innerText = 'El tiempo no debe superar los 180 minutos.';
+        return;
+    }
+
+    if (!Number.isInteger(Number(customTimeWork)) || !Number.isInteger(Number(customTimeRest))) {
+        error.classList.add('show');
+        error.innerText = 'Los tiempos deben ser números enteros.';
+        return;
+    }
+
+    error.classList.remove('show');
+    closeModal();
+}
+
+
+/* Timer */
 let timerInterval;
 let isPaused = false;
 let timeRemaining;
