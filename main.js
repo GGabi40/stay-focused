dd391cc2dabf815eb71632ebf16bb575409d1591
ggabi40 <gabibc2000@hotmail.com>
2025-03-12 11:06:42 -0300

    ✅ loading page added | ✅ Menu changes

--- main.js
+++ main.js
@@ -1,25 +1,105 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, Menu } from 'electron';
 import { resolve, dirname } from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const createWindow = () => {
-    const win = new BrowserWindow({
+let loadingWindow;
+let mainWindow;
+
+const createLoadingWindow = () => {
+    loadingWindow = new BrowserWindow({
+        width: 300,
+        height: 400,
+        frame: false,
+        alwaysOnTop: true,
+        resizable: false,
+        icon: resolve(__dirname, "assets", "img", "icon256x256px.png"),
+    });
+
+    loadingWindow.loadFile(resolve(__dirname, "loading.html"));
+};
+
+const createMainWindow = () => {
+    mainWindow = new BrowserWindow({
         width:400,
         height: 600,
         alwaysOnTop: true,
         transparent: true,
+        show: false, // evita que aparezca mientras se cargue
         webPreferences: {
             nodeIntegration: true
         },
         icon: resolve(__dirname, "assets", "img", "icon256x256px.png") // windows icon
     })
 
-    win.loadFile('index.html');
-}
+    mainWindow.loadFile('index.html');
 
+    // Cuando se cargue, cierra loading, abre main
+    mainWindow.webContents.once("did-finish-load", () => {
+        loadingWindow.close();
+        mainWindow.show();
+    });
+
+    // Cierra de segundo plano
+    mainWindow.on("closed", () => {
+        mainWindow = null;
+    });
+};
+
+// App lista
 app.whenReady().then(() => {
-    createWindow();
+    createLoadingWindow();
+
+    // Menu:
+    const mainMenu = Menu.buildFromTemplate(menu);
+    Menu.setApplicationMenu(mainMenu);
+
+    setTimeout(createMainWindow, 2000);
 });
+
+// asegura de que no se ejecute en 2o plano
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
+
+const menu = [
+    {
+        label: 'Archivo',
+        submenu: [
+            {
+                label: 'Salir',
+                click: () => app.quit()
+            }
+        ],
+    },
+    {
+        label: 'Vista',
+        submenu: [
+            {
+                label: 'Recargar',
+                click: () => {
+                    const focusedWindow = BrowserWindow.getFocusedWindow();
+                    if(focusedWindow) { focusedWindow.reload() };
+                }
+            },
+            {
+                label: 'Pantalla Completa',
+                click: () => {
+                    const focusedWindow = BrowserWindow.getFocusedWindow();
+                    if(focusedWindow) { focusedWindow.setFullScreen(!focusedWindow.isFullScreen()) };
+                }
+            },
+            {
+                label: 'Salir de Pantalla Completa',
+                click: () => {
+                    const focusedWindow = BrowserWindow.getFocusedWindow();
+                    if(focusedWindow) { focusedWindow.setFullScreen(false) };
+                }
+            }
+        ]
+    }
+]
